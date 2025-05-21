f6a9ecc1e4f5f6326d670e3d64a364c08808bcb8
Devendra <devendra.991023@gmail.com>
2025-05-21 07:43:10 +0530

    variables started

--- 04-read-command.sh
+++ 04-read-command.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Hi please enter your name"
-read name
+read -s name
 echo " Thankyou for entring your name $name"
 
 echo " enter your age $name"
-read age 
+read -s age 
 echo "your age is $age"
