2fcafc60df35fa9ba29ff498950f9ae192279266
Devendra <devendra.991023@gmail.com>
2025-05-21 07:53:09 +0530

    variables started

---
+++ 06-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+movies=("pushpa" "guntur kaaram" "game changer" "devara")
+
+echo "first movie is ${movies[0]}"
+echo "second movie is ${novies[1]}"
+
+echo "all movies are ${moies[@]}"
