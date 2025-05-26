c72ba0914d6303caa22222f5585b8a07b2c24e12
Devendra <devendra.991023@gmail.com>
2025-05-26 20:39:02 +0530

    special variables started

---
+++ 09-installing-packages.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+userid=$(id -u)
+if [ $userid -ne 0 ]
+then 
+    echo "please run this script with root access" 
+fi
+dnf install mysql -y
+
+if [ $? -eq 0 ]
+then
+    echo " mysql installed "
+else 
+    echo " mysql not installed "
+fi
