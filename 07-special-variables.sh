6cd8d615ab9d537ef3ffddaabbadf1ba96a5b3d5
Devendra <devendra.991023@gmail.com>
2025-05-26 20:27:07 +0530

    special variables started

--- 07-special-variables.sh
+++ 07-special-variables.sh
@@ -2,11 +2,11 @@
 
 
 variables=("nani" "dev" "devendra")
-echo "all variables passed to thi sscript : $@ "
-echo "number of variables passed : $#  "
-echo " script name : $0 "
+echo "all variables passed to this sscript : $@ "
+echo "number of variables passed : $#"
+echo "script name : $0 "
 echo "current directory : $PWD "
-echo " home directory of the user running the script : $HOME "
-echo : process ID of the script : $$ "
+echo "home directory of the user running the script : $HOME "
+echo : "process ID of the script : $$ "
 sleep 45 &
 echo "Process ID of the last command in background : $! "
