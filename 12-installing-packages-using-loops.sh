f81eb853ebd8dfe083be21b25e9f90ebe8a84bde
Devendra <devendra.991023@gmail.com>
2025-06-17 05:20:42 +0530

    instaling packages usimg loops in shell

--- 12-installing-packages-using-loops.sh
+++ 12-installing-packages-using-loops.sh
@@ -11,36 +11,36 @@ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 PACKAGES=("mysql" "python3" "nginx" "httpd")
 mkdir -p $LOGS_FOLDER
 
-echo "Script started at $(date)" | tee -a $LOG_FILE
+echo "Script started at $(date)"
 
 if [ $userid -ne 0 ]
 then 
-    echo -e "$r error : please run this script with root access $n" | tee -a $LOG_FILE
+    echo -e "$r error : please run this script with root access $n" 
     exit 1
 else
-    echo -e " you're running this script with root access" |tee -a $LOG_FILE
+    echo -e " you're running this script with root access" 
 fi
 
 VALIDATE(){
 if [ $1 -eq 0 ]
 then 
-    echo -e " installng $2 is ......$g SUCCESS $n " | tee -a $LOG_FILE
+    echo -e " installng $2 is ......$g SUCCESS $n "
 else 
-    echo -e "Installing $2 is ......$r failed $n " | tee -a $LOG_FILE
+    echo -e "Installing $2 is ......$r failed $n " 
     exit 1
 fi    
 }
 
 for package in ${PACKAGES[@]}
 do 
-    dnf list installed $package | tee -a $LOG_FILE
+    dnf list installed $package 
     if [ $? -ne 0 ]
     then 
-        echo " $package is not installed.....instaling it " |tee -a $LOG_FILE
+        echo " $package is not installed.....instaling it " 
         dnf install $package -y &>> $LOG_FILE  
         VALIDATE $? "package"
     else
-        echo -e "nothing to do $package is $y already installed $n " | tee -a $LOG_FILE
+        echo -e "nothing to do $package is $y already installed $n " 
     fi 
 done
 
